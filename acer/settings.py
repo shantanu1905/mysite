00a72c53b3bb92b044d1ed68dbbd042ee4b0f46c
shantanu1905 <nimkarshantanu07@gmail.com>
2021-09-21 23:27:06 +0530

    static

--- acer/settings.py
+++ acer/settings.py
@@ -140,14 +140,8 @@ EMAIL_HOST_PASSWORD =config('email_password')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
-# Extra lookup directories for collectstatic to find static files
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 #  Add configuration for static files storage using whitenoise
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
