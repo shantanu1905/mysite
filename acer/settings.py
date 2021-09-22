4b89df89d3e750548bb2130d3b25fa1829c013a1
shantanu1905 <nimkarshantanu07@gmail.com>
2021-09-22 11:40:33 +0530

    settings

--- acer/settings.py
+++ acer/settings.py
@@ -146,4 +146,4 @@ EMAIL_HOST_PASSWORD =os.environ['EPASS']
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
