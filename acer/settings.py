9e43b947190ba4891df2bd28deb230b06b9777a7
shantanu nimkar <59206895+shantanu1905@users.noreply.github.com>
2021-09-24 22:14:26 +0530

    smtp updated

--- acer/settings.py
+++ acer/settings.py
@@ -134,7 +134,7 @@ STATIC_ROOT = "/mysite/static/"
 
 #EMAIL CONFIGURATION
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_USE_TLS = True
 EMAIL_PORT = '587'
 EMAIL_HOST_USER = config('email')   
